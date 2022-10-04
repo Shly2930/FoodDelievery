9880efdc336f1609dd424f47e34dc4a6aeb466f4
Vivek Sehrawat <>
2022-10-04 20:20:38 +0530

    fixed overflow issue

--- fooddelivery/lib/screen/login_screen.dart
+++ fooddelivery/lib/screen/login_screen.dart
@@ -19,174 +19,181 @@ class LoginPage extends StatelessWidget {
       },
       child: Scaffold(
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-        body: Column(
-          children: [
-            Expanded(
-              child: Column(
-                children: [
-                  Stack(
+        body: SingleChildScrollView(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 0.9,
+            child: Column(
+              children: [
+                Expanded(
+                  child: Column(
                     children: [
-                      Container(
-                        child: Image.asset(AppImages.foodBanner),
+                      Stack(
+                        children: [
+                          Container(
+                            child: Image.asset(AppImages.foodBanner),
+                          ),
+                          Positioned(
+                              top: 40,
+                              right: 20,
+                              child: SmallRoundButton(
+                                  title: 'Skip',
+                                  onPressed: () {
+                                    print('Skip Clicked');
+                                  })),
+                        ],
                       ),
-                      Positioned(
-                          top: 40,
-                          right: 20,
-                          child: SmallRoundButton(
-                              title: 'Skip',
-                              onPressed: () {
-                                print('Skip Clicked');
-                              })),
-                    ],
-                  ),
-                  Container(
-                    color: theme.isDark ? Colors.blue : Colors.white,
-                    child: Column(children: [
+                      Container(
+                        color: theme.isDark ? Colors.blue : Colors.white,
+                        child: Column(children: [
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                top: 20, left: 20, right: 20),
+                            child: Text(
+                              "India's #1 Food Delivery and Dining App",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 25,
+                                  height: 1.5,
+                                  color: Colors.grey.shade900),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Expanded(
+                                child: new Container(
+                                    margin: const EdgeInsets.only(
+                                        left: 15.0, right: 15.0),
+                                    child: Divider(
+                                      color: Colors.black,
+                                      height: 36,
+                                    )),
+                              ),
+                              Container(
+                                child: Text(
+                                  'Log in or sign up',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                    height: 1.5,
+                                    color: Colors.grey.shade600,
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                child: new Container(
+                                    margin: const EdgeInsets.only(
+                                        left: 15.0, right: 15.0),
+                                    child: Divider(
+                                      color: Colors.black,
+                                      height: 36,
+                                    )),
+                              ),
+                            ],
+                          )
+                        ]),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
                       Padding(
-                        padding:
-                            const EdgeInsets.only(top: 20, left: 20, right: 20),
-                        child: Text(
-                          "India's #1 Food Delivery and Dining App",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 25,
-                              height: 1.5,
-                              color: Colors.grey.shade900),
+                        padding: const EdgeInsets.only(left: 30, right: 30),
+                        child: TextFormField(
+                          cursorColor: Colors.red.shade400,
+                          keyboardType: TextInputType.number,
+                          inputFormatters: [
+                            FilteringTextInputFormatter(RegExp("[0-9]"),
+                                allow: true),
+                          ],
+                          decoration: InputDecoration(
+                              hintText: 'Enter mobile number',
+                              enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    width: 1,
+                                  ),
+                                  borderRadius: BorderRadius.circular(10))),
                         ),
                       ),
                       SizedBox(
                         height: 20,
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Expanded(
-                            child: new Container(
-                                margin: const EdgeInsets.only(
-                                    left: 15.0, right: 15.0),
-                                child: Divider(
-                                  color: Colors.black,
-                                  height: 36,
-                                )),
-                          ),
-                          Container(
-                            child: Text(
-                              'Log in or sign up',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20,
-                                height: 1.5,
-                                color: Colors.grey.shade600,
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                            child: new Container(
-                                margin: const EdgeInsets.only(
-                                    left: 15.0, right: 15.0),
-                                child: Divider(
-                                  color: Colors.black,
-                                  height: 36,
-                                )),
-                          ),
-                        ],
+                      Padding(
+                          padding: const EdgeInsets.only(left: 30, right: 30),
+                          child: Container(
+                              child: AppMainButton(
+                                  title: 'Continue',
+                                  onPressed: () {
+                                    Navigator.pushNamed(context, "/homePage");
+                                  }))),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 130, right: 130),
+                        child: Row(
+                          children: [
+                            Text('Switch Theme'),
+                            Switch(
+                                value: theme.isDark,
+                                onChanged: ((val) {
+                                  theme.changeTheme();
+                                }))
+                          ],
+                        ),
                       )
-                    ]),
+                    ],
                   ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 30, right: 30),
-                    child: TextFormField(
-                      cursorColor: Colors.red.shade400,
-                      keyboardType: TextInputType.number,
-                      inputFormatters: [
-                        FilteringTextInputFormatter(RegExp("[0-9]"),
-                            allow: true),
-                      ],
-                      decoration: InputDecoration(
-                          hintText: 'Enter mobile number',
-                          enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                width: 1,
-                              ),
-                              borderRadius: BorderRadius.circular(10))),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Padding(
-                      padding: const EdgeInsets.only(left: 30, right: 30),
-                      child: Container(
-                          child: AppMainButton(
-                              title: 'Continue',
-                              onPressed: () {
-                                Navigator.pushNamed(context, "/homePage");
-                              }))),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 130, right: 130),
-                    child: Row(
-                      children: [
-                        Text('Switch Theme'),
-                        Switch(
-                            value: theme.isDark,
-                            onChanged: ((val) {
-                              theme.changeTheme();
-                            }))
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              height: 70,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'By Continuing, you agree to our',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 30.0, right: 30),
-                    child: SizedBox(
-                      height: 30,
-                      child: Row(
-                        children: [
-                          MaterialButton(
-                            onPressed: () {},
-                            child: Text(
-                              //https://www.zomato.com/policies/terms-of-service/
-                              'Terms of Service',
-                              style: TextStyle(fontSize: 10),
-                            ),
-                          ),
-                          MaterialButton(
-                            onPressed: () {},
-                            //https://www.zomato.com/policies/privacy
-                            child: Text('Privacy Policy',
-                                style: TextStyle(fontSize: 10)),
-                          ),
-                          MaterialButton(
-                            //https://www.zomato.com/policies/
-                            onPressed: () {},
-                            child: Text('Content Policies',
-                                style: TextStyle(fontSize: 10)),
-                          ),
-                        ],
+                ),
+                Container(
+                  height: 70,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'By Continuing, you agree to our',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 12),
                       ),
-                    ),
-                  )
-                ],
-              ),
-            )
-          ],
+                      Padding(
+                        padding: const EdgeInsets.only(left: 30.0, right: 30),
+                        child: SizedBox(
+                          height: 30,
+                          child: Row(
+                            children: [
+                              MaterialButton(
+                                onPressed: () {},
+                                child: Text(
+                                  //https://www.zomato.com/policies/terms-of-service/
+                                  'Terms of Service',
+                                  style: TextStyle(fontSize: 10),
+                                ),
+                              ),
+                              MaterialButton(
+                                onPressed: () {},
+                                //https://www.zomato.com/policies/privacy
+                                child: Text('Privacy Policy',
+                                    style: TextStyle(fontSize: 10)),
+                              ),
+                              MaterialButton(
+                                //https://www.zomato.com/policies/
+                                onPressed: () {},
+                                child: Text('Content Policies',
+                                    style: TextStyle(fontSize: 10)),
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
